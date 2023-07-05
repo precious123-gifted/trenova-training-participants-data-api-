db0b6db4dba94b5ee185b6c9e64fc76e5079515a
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-07-05 13:04:15 +0100

    Jul 5 13:04

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,56 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import { hash } from "bcryptjs"
 import { connectToMongoDB } from "../../../lib/mongodb/mongodb"
 import User from "../../../models/user"
+import { IUser } from "../../../types"
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
+
+
+
+const handler = async (req, res) => {
   try {
     await connectToMongoDB();
 
     if (req.method === "GET") {
       const users = await User.find().exec();
-      const userJSON = users.map((user) => {
-        const { participantName, schoolName, address, phoneNumber, email, _id, createdDate } = user;
-
-        let propertyValue;
-
-        switch (req.url) {
-          case "/participantName":
-            propertyValue = participantName;
-            break;
-          case "/schoolName":
-            propertyValue = schoolName;
-            break;
-          case "/address":
-            propertyValue = address;
-            break;
-          case "/phoneNumber":
-            propertyValue = phoneNumber;
-            break;
-          case "/email":
-            propertyValue = email;
-            break;
-          case "/_id":
-            propertyValue = _id;
-            break;
-          case "/createdDate":
-            propertyValue = createdDate;
-            break;
-          default:
-            // Return all properties
-            return {
-              participantName,
-              schoolName,
-              address,
-              phoneNumber,
-              email,
-              _id,
-              createdDate,
-            };
-        }
-
-        // Return the property value for the matched endpoint
-        return { [req.url]: propertyValue };
-      });
+      const userJSON = users.map((user) => ({
+       
+        phoneNumber: user.phoneNumber,
+       
+      }));
 
       res
         .status(200)
@@ -123,3 +91,101 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 };
 
 export default handler;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     await connectToMongoDB();
+
+//     if (req.method === "POST") {
+//       if (!req.body) return res.status(400).json({ error: "Data is missing" })
+
+//       const { participantName, schoolName, address, phoneNumber,email,createdDate } = req.body
+
+    
+
+  
+
+
+//         const user = new User({
+//           participantName,
+//           schoolName,
+//           address,
+//           phoneNumber,
+//           email,
+//           createdDate,
+//         })
+
+//         user.save()
+//           .then((data: IUser) => {
+//             const user = {
+//               participantName: data.participantName,
+//               schoolName: data.schoolName,
+//               address: data.address,
+//               phoneNumber:Number ,
+//               email: data.email,
+//               _id: data._id,
+//               createdDate: data.createdDate,
+//             }
+
+//             return res.status(201)
+//               .setHeader('Access-Control-Allow-Origin', 'https://trenova-training-participants-data-api-3ccr.vercel.app/') // Replace with your domain name
+//               .setHeader('Access-Control-Allow-Credentials', 'true')
+//               .json({
+//                 success: true,
+//                 user
+//               })
+//           })
+//           .catch((error: unknown) => {
+//             // handle any errors
+//             console.error(error);
+//             res.status(500)
+//               .setHeader('Access-Control-Allow-Origin', 'https://trenova-training-participants-data-api-3ccr.vercel.app/') // Replace with your domain name
+//               .setHeader('Access-Control-Allow-Credentials', 'true')
+//               .json({ error: "Internal Server Error" });
+//           })
+      
+//     } else if (req.method === "OPTIONS") {
+//       res.setHeader('Access-Control-Allow-Origin', 'https://trenova-training-participants-data-api-3ccr.vercel.app/'); // Replace with your domain name
+//       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//       res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+//       res.setHeader('Access-Control-Allow-Credentials', 'true')
+//       res.status(200).end();
+//     } else {
+//       res.status(405)
+//         .setHeader('Access-Control-Allow-Origin', 'https://trenova-training-participants-data-api-3ccr.vercel.app/') // Replace with your domain name
+//         .setHeader('Access-Control-Allow-Credentials', 'true')                           
+//         .json({ error: "Method Not Allowed,nor try this method again" })
+//     }
+//   } catch (error) {
+//     // handle any errors that occur during database connection
+//     console.error(error);
+//     res.status(500)
+//       .setHeader('Access-Control-Allow-Origin', 'https://trenova-training-participants-data-api-3ccr.vercel.app/') // Replace with your domain name
+//       .setHeader('Access-Control-Allow-Credentials', 'true')
+//       .json({ error: "Internal Server Error" });
+//    }
+// }
+
+// export default handler
+
+
+
+
