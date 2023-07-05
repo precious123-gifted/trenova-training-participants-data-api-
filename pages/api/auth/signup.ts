64d76e19a773d4c3453dd99163ed47838e670c9e
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-07-05 12:55:14 +0100

    Jul 5 12:55

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -8,15 +8,49 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (req.method === "GET") {
       const users = await User.find().exec();
-      const userJSON = users.map((user) => ({
-        participantName: user.participantName,
-        schoolName: user.schoolName,
-        address: user.address,
-        phoneNumber: user.phoneNumber,
-        email: user.email,
-        _id: user._id,
-        createdDate: user.createdDate,
-      }));
+      const userJSON = users.map((user) => {
+        const { participantName, schoolName, address, phoneNumber, email, _id, createdDate } = user;
+
+        let propertyValue;
+
+        switch (req.url) {
+          case "/participantName":
+            propertyValue = participantName;
+            break;
+          case "/schoolName":
+            propertyValue = schoolName;
+            break;
+          case "/address":
+            propertyValue = address;
+            break;
+          case "/phoneNumber":
+            propertyValue = phoneNumber;
+            break;
+          case "/email":
+            propertyValue = email;
+            break;
+          case "/_id":
+            propertyValue = _id;
+            break;
+          case "/createdDate":
+            propertyValue = createdDate;
+            break;
+          default:
+            // Return all properties
+            return {
+              participantName,
+              schoolName,
+              address,
+              phoneNumber,
+              email,
+              _id,
+              createdDate,
+            };
+        }
+
+        // Return the property value for the matched endpoint
+        return { [req.url]: propertyValue };
+      });
 
       res
         .status(200)
